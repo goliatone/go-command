4d50bf0e53cd9dc45bbb33a0beb16e8839b137b6
goliatone <140582+goliatone@users.noreply.github.com>
2024-12-22 13:47:28 -0800

    refactor: rename MessageError to Error so we can use it in the package

--- errors.go
+++ errors.go
@@ -4,27 +4,27 @@ import (
 	"fmt"
 )
 
-// MessageError is a custom error type wrapping context around dispatch failures
-type MessageError struct {
+// Error is a custom error type wrapping context around dispatch failures
+type Error struct {
 	Type    string
 	Message string
 	Err     error
 }
 
-func (e *MessageError) Error() string {
+func (e *Error) Error() string {
 	if e.Err != nil {
 		return fmt.Sprintf("%s: %s: %v", e.Type, e.Message, e.Err)
 	}
 	return fmt.Sprintf("%s: %s", e.Type, e.Message)
 }
 
-func (e *MessageError) Unwrap() error {
+func (e *Error) Unwrap() error {
 	return e.Err
 }
 
-// WrapMessageError is a helper to create wrapped errors using MessageError
-func WrapMessageError(errType, msg string, err error) *MessageError {
-	return &MessageError{
+// WrapError is a helper to create wrapped errors using MessageError
+func WrapError(errType, msg string, err error) *Error {
+	return &Error{
 		Type:    errType,
 		Message: msg,
 		Err:     err,
