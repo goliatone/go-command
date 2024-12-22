0e23eca5af7e81b58fb85c9c34e06d44e7a47a96
goliatone <140582+goliatone@users.noreply.github.com>
2024-12-22 15:35:03 -0800

    add: runner options

---
+++ runner/options.go
@@ -0,0 +1,66 @@
+package runner
+
+import "time"
+
+type Option func(*Handler)
+
+func WithTimeout(t time.Duration) Option {
+	return func(r *Handler) {
+		r.timeout = t
+	}
+}
+
+func WithDeadline(d time.Time) Option {
+	return func(r *Handler) {
+		r.deadline = d
+	}
+}
+
+func WithRunOnce(once bool) Option {
+	return func(r *Handler) {
+		r.once = once
+	}
+}
+
+func WithMaxRetries(max int) Option {
+	return func(r *Handler) {
+		r.maxRetries = max
+	}
+}
+
+func WithMaxRuns(max int) Option {
+	return func(r *Handler) {
+		r.maxRuns = max
+	}
+}
+
+func WithErrorHandler(h func(error)) Option {
+	return func(r *Handler) {
+		if h == nil {
+			h = func(err error) {}
+		}
+		r.errorHandler = h
+	}
+}
+
+func WithLogger(l Logger) Option {
+	return func(r *Handler) {
+		r.logger = l
+	}
+}
+
+func WithDoneHandler(d func(*Handler)) Option {
+	return func(r *Handler) {
+		if d == nil {
+			d = func(r *Handler) {}
+		}
+		r.doneHandler = d
+	}
+}
+
+// WithRetryStrategy lets you define a custom retry/backoff approach.
+func WithRetryStrategy(s RetryStrategy) Option {
+	return func(r *Handler) {
+		r.retryStrategy = s
+	}
+}
