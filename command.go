cb20d9405c8b3c0b651d919c9e135baa5c5bc9ca
goliatone <140582+goliatone@users.noreply.github.com>
2025-03-17 20:47:59 -0700

    refactor: move handler config to top level

--- command.go
+++ command.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"context"
+	"time"
 )
 
 // CommandFunc is an adapter that lets you use a function as a CommandHandler[T]
@@ -28,4 +29,13 @@ func (f QueryFunc[T, R]) Query(ctx context.Context, msg T) (R, error) {
 // Querier is responsible for returning data, with no side effects
 type Querier[T Message, R any] interface {
 	Query(ctx context.Context, msg T) (R, error)
+}
+
+type HandlerConfig struct {
+	Timeout    time.Duration `json:"timeout"`
+	Deadline   time.Time     `json:"deadline"`
+	MaxRetries int           `json:"max_retries"`
+	MaxRuns    int           `json:"max_runs"`
+	RunOnce    bool          `json:"run_once"`
+	Expression string        `json:"expression"`
 }
