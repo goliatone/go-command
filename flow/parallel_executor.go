9afd948b34962a966907686026b1aa72cfe7803a
goliatone <140582+goliatone@users.noreply.github.com>
2025-05-15 14:53:29 -0700

    add: new error info

--- flow/parallel_executor.go
+++ flow/parallel_executor.go
@@ -2,11 +2,13 @@ package flow
 
 import (
 	"context"
-	"errors"
+
+	"fmt"
 	"sync"
 
 	"github.com/goliatone/go-command"
 	"github.com/goliatone/go-command/runner"
+	"github.com/goliatone/go-errors"
 )
 
 type ParallelExecutor[T any] struct {
@@ -38,11 +40,20 @@ func (p *ParallelExecutor[T]) Execute(ctx context.Context, msg T) error {
 			h := runner.NewHandler(p.options...)
 
 			if err := runner.RunCommand(runCtx, h, cmd, msg); err != nil {
-				errs[index] = command.WrapError(
-					"ParallelExecutor",
-					"handler failed in paralled execution",
+				errs[index] = errors.Wrap(
 					err,
-				)
+					errors.CategoryHandler,
+					"handler failed in parallel execution",
+				).
+					WithTextCode("PARALLEL_EXECUTION_FAILED").
+					WithMetadata(map[string]any{
+						"handler_index":     index,
+						"handler_type":      fmt.Sprintf("%T", cmd),
+						"message_type":      command.GetMessageType(msg),
+						"total_handlers":    len(p.handlers),
+						"stop_on_error":     h.ShouldStopOnErr(),
+						"context_cancelled": runCtx.Err() != nil,
+					})
 
 				if h.ShouldStopOnErr() {
 					cancel()
@@ -53,10 +64,29 @@ func (p *ParallelExecutor[T]) Execute(ctx context.Context, msg T) error {
 	wg.Wait()
 
 	var finalErr error
+	var successCount int
 	for _, err := range errs {
 		if err != nil {
 			finalErr = errors.Join(finalErr, err)
+		} else {
+			successCount++
 		}
+	}
+
+	if finalErr != nil {
+		finalErr = errors.Wrap(
+			finalErr,
+			errors.CategoryHandler,
+			fmt.Sprintf("parallel execution completed with %d failures out of %d handlers",
+				len(p.handlers)-successCount, len(p.handlers)),
+		).
+			WithTextCode("PARALLEL_EXECUTION_SUMMARY").
+			WithMetadata(map[string]any{
+				"total_handlers":   len(p.handlers),
+				"successful_count": successCount,
+				"failed_count":     len(p.handlers) - successCount,
+				"message_type":     command.GetMessageType(msg),
+			})
 	}
 
 	return finalErr
