90cc5a45b7578602263b7ddd1d1efe2e67b91b51
goliatone <140582+goliatone@users.noreply.github.com>
2025-05-15 14:52:00 -0700

    refactor: move getType as GetMessageType to command

--- command.go
+++ command.go
@@ -2,6 +2,9 @@ package command
 
 import (
 	"context"
+	"reflect"
+	"regexp"
+	"strings"
 	"time"
 )
 
@@ -39,4 +42,50 @@ type HandlerConfig struct {
 	RunOnce    bool          `json:"run_once"`
 	Expression string        `json:"expression"`
 	NoTimeout  bool          `json:"no_timeout"`
+}
+
+func GetMessageType(msg any) string {
+	if msg == nil {
+		return "unknown_type"
+	}
+
+	v := reflect.ValueOf(msg)
+	if v.Kind() == reflect.Ptr && v.IsNil() {
+		return "unknown_type"
+	}
+
+	if msgTyper, ok := msg.(interface{ Type() string }); ok {
+		return msgTyper.Type()
+	}
+
+	t := reflect.TypeOf(msg)
+	if t == nil {
+		return "unknown_type"
+	}
+
+	typeName := t.String()
+
+	if t.Kind() == reflect.Ptr {
+		typeName = typeName[1:] // remove the "*" prefix
+		t = t.Elem()            // get the type that the pointer points to
+	}
+
+	pkgPath := t.PkgPath()
+	if pkgPath != "" {
+		parts := strings.Split(pkgPath, "/")
+		pkgPath = parts[len(parts)-1]
+	}
+
+	txName := toSnakeCase(typeName)
+
+	if pkgPath == "" {
+		return txName
+	}
+	return pkgPath + "::" + txName
+}
+
+func toSnakeCase(s string) string {
+	//TODO: use tocase package
+	snake := regexp.MustCompile("([a-z0-9])([A-Z])").ReplaceAllString(s, "${1}_${2}")
+	return strings.ToLower(snake)
 }
