ed44d61fcd895fbd5ce1225a3b1e4d05a0c7c6c9
goliatone <140582+goliatone@users.noreply.github.com>
2026-01-16 12:18:32 -0800

    fix: track subscriptions, reset dispatcher state

--- registry/registry.go
+++ registry/registry.go
@@ -2,6 +2,7 @@ package registry
 
 import (
 	"context"
+	"sync"
 
 	"github.com/alecthomas/kong"
 	"github.com/goliatone/go-command"
@@ -10,15 +11,27 @@ import (
 )
 
 var globalRegistry = command.NewRegistry()
+var globalSubsMu sync.Mutex
+var globalSubs []dispatcher.Subscription
 
 func RegisterCommand[T any](cmd command.Commander[T], runnerOpts ...runner.Option) (dispatcher.Subscription, error) {
 	sub := dispatcher.SubscribeCommand(cmd, runnerOpts...)
-	return sub, globalRegistry.RegisterCommand(cmd)
+	if err := globalRegistry.RegisterCommand(cmd); err != nil {
+		sub.Unsubscribe()
+		return nil, err
+	}
+	trackSubscription(sub)
+	return sub, nil
 }
 
 func RegisterQuery[T any, R any](qry command.Querier[T, R], runnerOpts ...runner.Option) (dispatcher.Subscription, error) {
 	sub := dispatcher.SubscribeQuery(qry, runnerOpts...)
-	return sub, globalRegistry.RegisterCommand(qry)
+	if err := globalRegistry.RegisterCommand(qry); err != nil {
+		sub.Unsubscribe()
+		return nil, err
+	}
+	trackSubscription(sub)
+	return sub, nil
 }
 
 func SetCronRegister(fn func(opts command.HandlerConfig, handler any) error) {
@@ -42,13 +55,54 @@ func Start(_ context.Context) error {
 }
 
 func Stop(_ context.Context) error {
+	unsubscribeAll()
 	globalRegistry = command.NewRegistry()
+	dispatcher.Reset()
 	return nil
 }
 
 func WithTestRegistry(fn func()) {
 	old := globalRegistry
+	oldSubs := stashSubscriptions()
 	defer func() { globalRegistry = old }()
 	globalRegistry = command.NewRegistry()
 	fn()
+	unsubscribeAll()
+	restoreSubscriptions(oldSubs)
+}
+
+func trackSubscription(sub dispatcher.Subscription) {
+	if sub == nil {
+		return
+	}
+	globalSubsMu.Lock()
+	defer globalSubsMu.Unlock()
+	globalSubs = append(globalSubs, sub)
+}
+
+func stashSubscriptions() []dispatcher.Subscription {
+	globalSubsMu.Lock()
+	defer globalSubsMu.Unlock()
+	old := globalSubs
+	globalSubs = nil
+	return old
+}
+
+func restoreSubscriptions(subs []dispatcher.Subscription) {
+	globalSubsMu.Lock()
+	defer globalSubsMu.Unlock()
+	globalSubs = subs
+}
+
+func unsubscribeAll() {
+	globalSubsMu.Lock()
+	subs := globalSubs
+	globalSubs = nil
+	globalSubsMu.Unlock()
+
+	for _, sub := range subs {
+		if sub != nil {
+			sub.Unsubscribe()
+		}
+	}
 }
